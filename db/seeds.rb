a97307ab02c7323c9752a3cb5bef5e2297e72794
Christopher Quinn <caquinn.86@gmail.com>
2016-10-13 01:25:57 -0700

    set up base admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@ user.email = "caquinn.86@gmail.com"
 user.password = "a6d43bzz89dc"
 user.admin = true
 user.save!
+
+user = User.new
+user.email = "acanineintime@gmail.com"
+user.password = "a6d43bzz89dc"
+user.admin = true
+user.save!
