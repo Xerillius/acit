8b41557d3568b25263766175cfd27b52c2dafb34
Chris <caquinn.86@gmail.com>
2016-10-16 00:12:43 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,1 @@
-user = User.new
-user.email = "caquinn.86@gmail.com"
-user.password = "a6d43bzz89dc"
-user.admin = true
-user.save!
 
-user = User.new
-user.email = "acanineintime@gmail.com"
-user.password = "a6d43bzz89dc"
-user.admin = true
-user.save!
